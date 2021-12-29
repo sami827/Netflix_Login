710c3861e9cf791cbd554c0e34e0e348f383e2dc
Saubiya Jahan <saubiyajahan@gmail.com>
2021-12-29 14:46:07 +0600

    finalized project

--- java/NetflixLogin.java
+++ java/NetflixLogin.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class NetflixLogin {
@@ -22,9 +21,10 @@ public class NetflixLogin {
     String PASSWORD_FIELD = "id_password"; //ID of Password Field
     String SIGN_IN_BUTTON = "//button[text()='Sign In']"; //Xpath of sign in button
     String LOGIN_FAILED_TEXT = "//div[@class='ui-message-contents']"; //Xpath of unsuccessful text
+    String LOGIN_SUCCESSFUL_TEXT = "//span[@class='our-story-welcome-back']"; //Xpath of successful text
+    String INPUT_FIELD_ERROR = "div.inputError"; //CSS of input error text
 
-
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -34,8 +34,55 @@ public class NetflixLogin {
         driver.get(baseUrl);
     }
 
-    @Test
-    public void loginToNetflix() {
+    @Test(priority = 1)
+    public void validateField() throws InterruptedException {
+
+
+        System.out.println("Successfully loaded the website");
+
+        //Homepage
+        //Verifying home page
+        WebElement netflixText = driver.findElement(By.id(NETFLIX_LOGO));
+        Assert.assertTrue(netflixText.isDisplayed());
+        System.out.println("Verified Page");
+
+        //Clicking login button
+        WebElement loginBtn = driver.findElement(By.xpath(LOGIN_BTN));
+        loginBtn.click();
+        System.out.println("Clicked on Login Button");
+
+        //Sign In Page
+        System.out.println("Navigating to Sign in page");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Verify sign in page
+        WebElement signInText = driver.findElement(By.xpath(SIGN_IN_TEXT));
+        Assert.assertTrue(signInText.isDisplayed());
+        System.out.println("Sign in Page Verified");
+
+        //Working with email field
+
+        WebElement emailField = driver.findElement(By.id(EMAIL_FIELD));
+        emailField.clear();
+        emailField.sendKeys("88");
+        Thread.sleep(2000);
+        emailField.clear();
+        Thread.sleep(2000);
+        emailField.sendKeys("abc");
+
+        //Handling input field error
+        WebElement inputError = driver.findElement(By.cssSelector(INPUT_FIELD_ERROR));
+
+        if (inputError.isDisplayed()) {
+            System.out.println("Value not acceptable");
+        }
+
+    }
+
+
+    //Unsuccessful login test case
+    @Test(priority = 2)
+    public void loginFailTest() {
         System.out.println("Successfully loaded the website");
 
         //Homepage
@@ -63,7 +110,7 @@ public class NetflixLogin {
         //Working with email field
         WebElement emailField = driver.findElement(By.id(EMAIL_FIELD));
         emailField.clear();
-        emailField.sendKeys("samialzsabbir@gmail.com");
+        emailField.sendKeys("abc123@gmail.com");
 
         //Working with password field
 
@@ -80,6 +127,59 @@ public class NetflixLogin {
         WebElement loginUIText = driver.findElement(By.xpath(LOGIN_FAILED_TEXT));
         if(loginUIText.isDisplayed()) {
             System.out.println("Please insert correct credentials");
+        }
+
+    }
+
+    //Successful login test
+    @Test(priority = 3)
+    public void loginSuccessTest() {
+        System.out.println("Successfully loaded the website");
+
+        //Homepage
+        //Verifying home page
+        WebElement netflixText = driver.findElement(By.id(NETFLIX_LOGO));
+        Assert.assertTrue(netflixText.isDisplayed());
+        System.out.println("Verified Page");
+
+        //Clicking login button
+        WebElement loginBtn = driver.findElement(By.xpath(LOGIN_BTN));
+        loginBtn.click();
+        System.out.println("Clicked on Login Button");
+
+        //Sign In Page
+        System.out.println("Navigating to Sign in page");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        //Verify sign in page
+        WebElement signInText = driver.findElement(By.xpath(SIGN_IN_TEXT));
+        Assert.assertTrue(signInText.isDisplayed());
+        System.out.println("Sign in Page Verified");
+
+
+        //Working with email field
+        WebElement emailField = driver.findElement(By.id(EMAIL_FIELD));
+        emailField.clear();
+        emailField.sendKeys("Hurvai7@gmail.com");
+
+        //Working with password field
+
+        WebElement passField = driver.findElement(By.id(PASSWORD_FIELD));
+        passField.clear();
+        passField.sendKeys("Hello123.");
+
+        //Click Login Button
+        WebElement signInButton = driver.findElement(By.xpath(SIGN_IN_BUTTON));
+        signInButton.click();
+        System.out.println("Clicked Sign in Button");
+
+        //Handling successful Login
+        WebElement loginUIText = driver.findElement(By.xpath(LOGIN_SUCCESSFUL_TEXT));
+        boolean flag = loginUIText.isDisplayed();
+        Assert.assertTrue(flag);
+        if(flag) {
+            System.out.println("Hooray! You've logged in successfully");
         }
 
     }
